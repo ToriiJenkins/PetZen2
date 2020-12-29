a418bd2dc625614702c50ce756d7fc2e4f9e6680
tjenkins <torii.j.jenkins@gmail.com>
2020-12-28 20:19:51 -0500

    Pet,Med,Food,Act,Feeding Pocos

---
+++ PetZen.Data/Pet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetZen.Data
+{
+    public class Pet
+    {
+        [Key]
+        public int PetId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public SpeciesEnum Species { get; set; }
+
+        public string Breed { get; set; }
+
+        public double? Weight { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required]
+        public int MealsPerDay { get; set; }
+
+    }
+}
--- PetZen.WebMVC/Controllers/AccountController.cs
+++ PetZen.WebMVC/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using PetZen.Data;
 using PetZen.WebMVC.Models;
 
 namespace PetZen.WebMVC.Controllers
